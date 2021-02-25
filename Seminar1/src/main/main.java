ececb2f04f39d9f7beaff87a9430e8255b55da38
Oana <danoana18@stud.ase.ro>
2021-02-25 08:55:47 +0200

    classes

--- Seminar1/src/main/main.java
+++ Seminar1/src/main/main.java
@@ -1,10 +1,17 @@
 package main;
 
+import classes.Zoo;
+import classes.Zookeeper;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		//System.out.println("Hello");
+		Zookeeper zookeeper = new Zookeeper("Dani");
+		Zoo zoo = new Zoo();
+		zoo.setZookeeper(zookeeper);
+		
 	}
 
 }
