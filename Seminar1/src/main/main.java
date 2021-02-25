c1d5d63c6e14daf07eda938fb942f5b91703c08f
Oana <danoana18@stud.ase.ro>
2021-02-25 09:13:39 +0200

    classes

--- Seminar1/src/main/main.java
+++ Seminar1/src/main/main.java
@@ -1,5 +1,7 @@
 package main;
 
+import classes.Giraffe;
+import classes.Zebra;
 import classes.Zoo;
 import classes.Zookeeper;
 
@@ -12,6 +14,21 @@ public class main {
 		Zoo zoo = new Zoo();
 		zoo.setZookeeper(zookeeper);
 		
+		Giraffe g1 = new Giraffe("Girafa", 10);
+		Giraffe g2 = new Giraffe("Girafa2", 15, 200);
+		
+		zoo.addAnimal(g1);
+		zoo.addAnimal(g2);
+		
+		zoo.feedAllAnimals();
+		
+		Zebra z1 = new Zebra("Zebra1", 7);
+		Zebra z2 = new Zebra("Zebra2", 9, 132);
+		
+		zoo.addAnimal(z1);
+		zoo.addAnimal(z2);
+		
+		zoo.feedAllAnimals();
 	}
 
 }
