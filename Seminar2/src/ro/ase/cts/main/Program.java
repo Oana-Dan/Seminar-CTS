096650a1cab33d28047d8ec350e3a18066de812c
Oana <danoana18@stud.ase.ro>
2021-03-04 08:24:37 +0200

    commit

--- Seminar2/src/ro/ase/cts/main/Program.java
+++ Seminar2/src/ro/ase/cts/main/Program.java
@@ -1,18 +1,17 @@
 package ro.ase.cts.main;
 
 import ro.ase.cts.clase.*;
+import ro.ase.cts.readers.AngajatReader;
+
 import java.io.FileNotFoundException;
-
 import java.util.List;
-
-import ro.ase.cts.utils.Utils;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
