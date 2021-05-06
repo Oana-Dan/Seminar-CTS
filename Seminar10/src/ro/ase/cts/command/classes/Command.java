d4ad8f83ce5a53867547e9e7eb63118b117be0ab
Oana <danoana18@stud.ase.ro>
2021-05-06 08:28:43 +0300

    Command

---
+++ Seminar10/src/ro/ase/cts/command/classes/Command.java
@@ -0,0 +1,21 @@
+package ro.ase.cts.command.classes;
+
+public abstract class Command {
+	private ContBancar cont;
+	private float suma;
+	public abstract void executa();
+	
+	public Command(ContBancar cont, float suma) {
+		super();
+		this.cont = cont;
+		this.suma = suma;
+	}
+
+	public ContBancar getCont() {
+		return cont;
+	}
+
+	public float getSuma() {
+		return suma;
+	}
+}
