845c6141ef6bced3ca265e3aab91ce71230e0902
Oana <danoana18@stud.ase.ro>
2021-05-06 09:13:44 +0300

    Chain

--- Seminar10/src/ro/ase/cts/command/classes/ManagerComenzi.java
+++ Seminar10/src/ro/ase/cts/command/classes/ManagerComenzi.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ManagerComenzi {
-	List<Command> comenzi;
+	private List<Command> comenzi;
 	
 	public ManagerComenzi() {
 		super();
